4cef28be4e1f57f0017568b0555c6f156b359b9d
xta <ni77ty@gmail.com>
2020-11-02 12:57:31 +0300

    .

--- TrainingPractice_01/KES_Task_01/Program.cs
+++ TrainingPractice_01/KES_Task_01/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,24 @@ namespace KES_Task_01
 {
     class Program
     {
+        public const int rate = 100;
         static void Main(string[] args)
         {
-            Console.WriteLine("aaa");
+            Console.WriteLine("Введите золото: ");
+            int gold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Сколько кристаллов вы хотите купить? Курс 1 кр. = {rate} золота");
+            int crystals = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                int[] arr = new int[gold+1];
+                int res = gold - crystals * rate;
+                arr[res] = 1;
+                Console.WriteLine($"У вас {crystals} кристаллов и {res} золота.");
+            }
+            catch (Exception e) {
+                Console.WriteLine($"Сделка не удалась - у вас {gold} золота и 0 кристаллов.");
+            };
         }
     }
 }
