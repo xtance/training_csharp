436c2e9cc738fedf77f369a809955bb09bf936b0
xta <ni77ty@gmail.com>
2020-11-02 13:44:59 +0300

    сделал второе задание))

---
+++ TrainingPractice_01/KES_Task_02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KES_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            do
+            {
+                str = Console.ReadLine();
+            } while (!str.Equals("exit"));
+        }
+    }
+}
