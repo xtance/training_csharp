8e811992a25b448d5689da4fc25742cbf82126f3
xta <ni77ty@gmail.com>
2020-11-06 01:22:15 +0300

    .

---
+++ TrainingPractice_01/KES_Task_07/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace KES_Task_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+            Random random = new Random();
+            for (int i = 0; i < size; i++) arr[i] = random.Next(0, 100);
+
+            Console.WriteLine("\nИзначальный массив:");
+            Print(arr, size);
+
+            // Перемешиваем массив
+            for (int i = size - 1; i > 0; i--) {
+                int j = random.Next(0, i + 1);
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+
+            Console.WriteLine("Перемешанный массив:");
+            Print(arr, size);
+        }
+
+        // Удобный вывод массива
+        static void Print(int[] arr, int size)
+        {
+            Console.Write("\n");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"{arr[i]}\t");
+                if ((i+1) % 5 == 0) Console.Write("\n");
+            }
+            Console.Write("\n");
+        }
+    }
+}
